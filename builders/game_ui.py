c84bfe3ad4f67ce043bb88803e5d7cd2af913b59
KJ <gresjay@gmail.com>
2026-03-25 07:20:59 +0700

    update rtp center fix

--- builders/game_ui.py
+++ builders/game_ui.py
@@ -71,7 +71,7 @@ def build_ui(data, play_button, show_provider=True):   # 🔥 เปลี่ย
                             "type": "image",
                             "url": data.get("provider_icon", ""),
                             "aspectRatio": "1:1",
-                            "size": "sm",
+                            "size": "xs",
                             "flex": 0
                         }
                     ] if show_provider else []),
